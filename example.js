6364c133a6a22f0f8547a88fd69459a9f9c1ba68
Kim, Hanjoon <kimhanjoon@gmail.com>
2015-05-09 01:48:50 +0900

    zone2 적용

--- example.js
+++ example.js
@@ -1,9 +1,18 @@
+var async = require('async');
 var fs = require('fs');
 var parser = require('./parser.js');
 
-fs.readFile('./example/ZONE2.html', 'utf8', function (err, data) {
-    if (err) {
-        return console.log(err);
-    }
-    console.log(parser.parse(data));
+var readFileUtf8 = function(filename, callback) {
+    return fs.readFile(filename, 'utf8', callback);
+};
+
+async.map(['./example/ZONE1.html','./example/ZONE2.html'], readFileUtf8, function(err, data){
+    // results is now an array of stats for each file
+    console.log(data.length);
+    console.log(data[0].length);
+    var menu1 = parser.parse(data[0]);
+    console.log(menu1.concat);
+    var menu2 = parser.parse(data[1]);
+//    console.log(menu2);
+    console.log(parser.render(menu1.concat(menu2)));
 });
