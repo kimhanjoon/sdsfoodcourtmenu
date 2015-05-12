578c7f6544308459788759c3e4bde02e72b63dea
kimhanjoon <kimhanjoon@gmail.com>
2015-05-12 13:10:15 +0900

    zonename으로 변경

--- example.js
+++ example.js
@@ -3,11 +3,11 @@ var fs = require('fs');
 
 var parser = require('./parser.js');
 
-var readFileUtf8 = function(filename, callback) {
-    return fs.readFile(filename, 'utf8', callback);
+var readFileUtf8 = function(zonename, callback) {
+    return fs.readFile('./example/' + zonename + '.html', 'utf8', callback);
 };
 
-async.map(['./example/ZONE1.html','./example/ZONE2.html'], readFileUtf8, function(err, data){
+async.map(['ZONE01','ZONE02'], readFileUtf8, function(err, data){
     var menu1 = parser.parse(data[0]);
     var menu2 = parser.parse(data[1]);
     var html = parser.render(menu1.concat(menu2));
