2f0c01bc1b4203b1e5f6c85a25ce09f4e5c7a447
kimhanjoon <kimhanjoon@gmail.com>
2015-05-12 12:50:21 +0900

    proxy 추가

--- gethtml.js
+++ gethtml.js
@@ -2,6 +2,7 @@ var request = require('request');
 var parser = require('./parser.js');
 var cheerio = require('cheerio');
 
+var request = request.defaults({'proxy':'http://70.10.15.10:8080'});
 request("http://www.sdsfoodmenu.co.kr:9106/foodcourt/menuplanner/list?zoneId=ZONE01", function(error, response, body) {
     console.log(parser.parse(body));
 });
