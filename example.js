3a459c9fd31c507c6ff343c3b02a4837a6c310a5
kimhanjoon <kimhanjoon@gmail.com>
2015-05-12 13:09:11 +0900

    underscore로 중복제거하는거 삭제

--- example.js
+++ example.js
@@ -1,6 +1,5 @@
 var async = require('async');
 var fs = require('fs');
-var _ = require('underscore');
 
 var parser = require('./parser.js');
 
@@ -9,12 +8,8 @@ var readFileUtf8 = function(filename, callback) {
 };
 
 async.map(['./example/ZONE1.html','./example/ZONE2.html'], readFileUtf8, function(err, data){
-    // results is now an array of stats for each file
     var menu1 = parser.parse(data[0]);
     var menu2 = parser.parse(data[1]);
-    var menu = _.uniq(menu1.concat(menu2), function(element) {
-        return element.title_kor;
-    });
-    var html = parser.render(menu);
+    var html = parser.render(menu1.concat(menu2));
     fs.writeFile('./example/MENU.html', html, 'utf8', function(){});
 });
