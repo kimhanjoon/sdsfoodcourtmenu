76adad33881344353aec3c8fc9bb3cf8890a54fa
Kim, Hanjoon <kimhanjoon@gmail.com>
2015-05-31 17:06:45 +0900

    예제 json 파일 추가

---
+++ json.js
@@ -0,0 +1,15 @@
+var async = require('async');
+var fs = require('fs');
+
+var parser = require('./parser.js');
+
+var readFileUtf8 = function(zonename, callback) {
+    return fs.readFile('./example/' + zonename + '.html', 'utf8', callback);
+};
+
+async.map(['ZONE01','ZONE02'], readFileUtf8, function(err, data){
+    var menu1 = parser.parse(data[0]);
+    var menu2 = parser.parse(data[1]);
+    var html = JSON.stringify(menu1.concat(menu2));
+    fs.writeFile('./public/menu.json', html, 'utf8', function(){});
+});
