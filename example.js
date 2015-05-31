8ea78157590372dfb2ff903728a23d4172a3a370
Kim, Hanjoon <kimhanjoon@gmail.com>
2015-06-01 01:03:01 +0900

    테스트결과파일명 변경, html과 json 한번에 생성

--- example.js
+++ example.js
@@ -1,15 +1,21 @@
-var async = require('async');
 var fs = require('fs');
-
-var parser = require('./parser.js');
-
 var readFileUtf8 = function(zonename, callback) {
-    return fs.readFile('./example/' + zonename + '.html', 'utf8', callback);
+    return fs.readFile('./jamsilmenu/' + zonename + '.html', 'utf8', callback);
 };
 
+var parser = require('./parser.js');
+var async = require('async');
 async.map(['ZONE01','ZONE02'], readFileUtf8, function(err, data){
-    var menu1 = parser.parse(data[0]);	console.log(menu1);
-    var menu2 = parser.parse(data[1]);	console.log(menu2);
-    var html = parser.render(menu1.concat(menu2));
-    fs.writeFile('./example/MENU.html', html, 'utf8', function(){});
+    var menu1 = parser.parse(data[0]);
+    var menu2 = parser.parse(data[1]);
+    
+    var menu = menu1.concat(menu2);
+    console.log(menu);
+    
+    var html = parser.render(menu);
+    fs.writeFile('./public/jamsilmenu.html', html, 'utf8', function(){});
+
+    var json = JSON.stringify(menu);
+    fs.writeFile('./public/jamsilmenu.json', json, 'utf8', function(){});
+    
 });
