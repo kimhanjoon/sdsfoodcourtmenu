969e383ecab325ca76f941fd92da4d6e968365f1
Kim, Hanjoon <kimhanjoon@gmail.com>
2015-12-10 00:48:50 +0900

    snapsnack 추가

--- example.js
+++ example.js
@@ -8,9 +8,22 @@ var async = require('async');
 async.map(['ZONE01','ZONE02'], readFileUtf8, function(err, data){
     var menu1 = parser.parse(data[0]);
     var menu2 = parser.parse(data[1]);
+    var menu3 = [
+                           {floor: "b1-snapsnack", title_kor:"델라면", title_eng:"Del Ramen", price: 3800}
+                           , {floor: "b1-snapsnack", title_kor:"떡라면", title_eng:"Ramen with Rice Cake", price: 4200}
+                           , {floor: "b1-snapsnack", title_kor:"떡라면", title_eng:"Ramen with Rice Cake", price: 4200}
+                           , {floor: "b1-snapsnack", title_kor:"떡라면", title_eng:"Ramen with Rice Cake", price: 4200}
+                           , {floor: "b1-snapsnack", title_kor:"떡라면", title_eng:"Ramen with Rice Cake", price: 4200}
+                           , {floor: "b1-snapsnack", title_kor:"떡라면", title_eng:"Ramen with Rice Cake", price: 4200}
+                           , {floor: "b1-snapsnack", title_kor:"떡라면", title_eng:"Ramen with Rice Cake", price: 4200}
+                           , {floor: "b1-snapsnack", title_kor:"떡라면", title_eng:"Ramen with Rice Cake", price: 4200}
+                           , {floor: "b1-snapsnack", title_kor:"떡라면", title_eng:"Ramen with Rice Cake", price: 4200}
+                           , {floor: "b1-snapsnack", title_kor:"떡라면", title_eng:"Ramen with Rice Cake", price: 4200}
+                		];
+                	    console.log(menu1.concat(menu2).concat(menu3));
     
     var option = {
-    		foods: menu1.concat(menu2)
+    		foods: menu1.concat(menu2).concat(menu3)
 //    		, production: argv.production
     		, language: 'en'
     };
