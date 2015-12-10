0e3a475bf539a0be14deed4d7f781937d870c637
Kim, Hanjoon <kimhanjoon@gmail.com>
2015-12-11 02:46:11 +0900

    image 폴더 변경

--- image_cache.js
+++ image_cache.js
@@ -6,25 +6,22 @@ var _ = require('underscore');
 var fs = require('fs');
 
 exports.init = function(logger) {
-	img_cache_map = _.chain(fs.readdirSync("./photo/"))
+	img_cache_map = _.chain(fs.readdirSync("./image/"))
 		.filter(function(e) {
-			return e.indexOf('jpg', e.length - 'jpg'.length) !== -1;
+			return e.indexOf("jpg") > -1;
 		})
 		.map(function(e) {
-			return e.substr(0, e.length - 3 - 1);
-		})
-		.map(function(e) {
-			return [e, "./photo/" + e + '.jpg'];
+			return [e.replace(".jpg", ""), "/image/" + e];
 		})
 		.object()
 		.value();
 	
 	if( logger && typeof logger === "function" ) {
-		console.info("logger set : ", logger);
+		console.info("logger set : ", logger.name);
 		_log = logger;
 	}
 	
-	console.info("image cached initially : ", Object.keys(img_cache_map).length);
+	console.info("image cache initialized : ", Object.keys(img_cache_map).length);
 };
 
 var request = require('request');
@@ -35,11 +32,11 @@ exports.put = function(food_id, food_img_src) {
 	
 	request({url: food_img_src, encoding: 'binary'}, function(error, response, body) {
 		_log('image requested : ', food_img_src);
-		fs.writeFile('./downloadphoto/' + food_id + '.png', body, 'binary', function(){
+		fs.writeFile('./downloadimage/' + food_id + '.png', body, 'binary', function(){
 			_log('image downloaded : ', food_id);
-			easyimg.convert({src:'./downloadphoto/' + food_id + '.png', dst: './photo/' + food_id + '.jpg', quality:60, background: 'white'}).then(function (file) {
+			easyimg.convert({src:'./downloadimage/' + food_id + '.png', dst: './image/' + food_id + '.jpg', quality:60, background: 'white'}).then(function (file) {
 				_log("image converted : ", file.path);
-				img_cache_map[food_id] = './photo/' + food_id + '.jpg';
+				img_cache_map[food_id] = '/image/' + food_id + '.jpg';
 				console.info("image cached initially : ", Object.keys(img_cache_map).length);
 			});
 		});
