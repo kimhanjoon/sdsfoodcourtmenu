f1a9d1cab78488f14c320c024f627fa676c1ad34
Kim, Hanjoon <kimhanjoon@gmail.com>
2016-08-11 00:40:36 +0900

    port를 config로 이동

---
+++ config/env/test.js
@@ -0,0 +1,4 @@
+module.exports = {
+	port: 5080,
+	googleAnalytics: false
+};
--- server.js
+++ server.js
@@ -2,14 +2,16 @@ require("console-stamp")(console, { pattern : "yyyy-mm-dd HH:MM:ss" } );
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'local';
 
+var config = require('./config/config');
+
 var imageConverter = require('./app/service/image.converter');
 
 var express = require('./config/express');
 
 var app = express();
 
-app.listen(4080);
+app.listen(config.port);
 
 module.exports = app;
 
-console.log("Server running at http://localhost:8080/");
+console.log("Server running at %d", config.port);
