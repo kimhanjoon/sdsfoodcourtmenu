6d93b97edee75c2552244999914780c79106d2e4
Kim, Hanjoon <kimhanjoon@gmail.com>
2016-11-03 00:12:47 +0900

    이미지 파일 없을 때 no image

--- config/express.js
+++ config/express.js
@@ -5,6 +5,7 @@ var compress = require('compression');
 var bodyParser = require('body-parser');
 var exphbs = require('express-handlebars');
 var Swag = require('swag');
+var path = require('path');
 
 module.exports = function() {
 
@@ -47,5 +48,9 @@ module.exports = function() {
 	require('../app/controller/umyeon.server.controller.js')(app);
 	require('../app/controller/sangam.server.controller.js')(app);
 
+	app.use(function(req, res, next) {
+		res.status(404).sendFile(path.join(__dirname, '/../public/image/no_image_available.jpg'));
+	});
+	
 	return app;
 };
